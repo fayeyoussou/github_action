c3e11f1df9a3fe78371a85df1701e321680d7f50
fayeyoussou <fayeyoussoupha@gmail.com>
2024-03-13 10:02:02 +0000

    Update HelloController.java

--- src/main/java/sn/youdev/github_action/controller/HelloController.java
+++ src/main/java/sn/youdev/github_action/controller/HelloController.java
@@ -1,6 +1,7 @@
 package sn.youdev.github_action.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +11,9 @@ public class HelloController {
     @GetMapping
     public String sayHello(){
         return "Hello";
+    }
+    @GetMapping("/name/{name}")
+    public String sayHello(@PathVariable final String name){
+        return "Hello"+name;
     }
 }
